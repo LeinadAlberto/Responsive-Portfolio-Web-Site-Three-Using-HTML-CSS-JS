86c077dd2912b5243bb2f770b9d4244ed5cd3f9a
Alberto <dany.canaviri2022@gmail.com>
2024-10-19 00:58:23 -0400

    Send email with emailjs, completed

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,36 @@
 /*=============== EMAIL JS ===============*/
+const contactForm = document.getElementById('contact-form'),
+      contactMessage = document.getElementById('contact-message')
+
+const sendEmail = (e) => {
+    e.preventDefault()  
+    
+    // serviceID - templateID - #form - publicKey
+    emailjs.sendForm('service_562pap9', 'template_y2jwfr9', '#contact-form', 'eu-1WTdUlXGNU-YMi')
+
+    .then(() => {
+        // Show sent message
+        contactMessage.textContent = 'Message sent successfully ✅'
+
+        // Remove message after five seconds
+        setTimeout(() => {
+            contactMessage.textContent = ''
+        }, 5000)
+
+        // Clear input fields
+        contactForm.reset()
+
+    }, () => {
+
+        // Show error message
+        contactMessage.textContent = 'Message not sent (service error) ❌'
+
+    })
+
+}
+
+contactForm.addEventListener('submit', sendEmail)
+
 
 
 /*=============== SHOW SCROLL UP ===============*/ 
